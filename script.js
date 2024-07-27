cf12320e4c716416dccbd00379d106eeee3bd191
Pedro Henrique <pedrohbc96@gmail.com>
2024-07-27 18:30:06 -0300

    Adiciona melhoria na movimentação da carta

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('mousemove', (event) => {
         const { clientX, clientY } = event;
         const { innerWidth, innerHeight } = window;
-        const moveX = ((clientX / innerWidth) - 0.5) * 25;
-        const moveY = ((clientY / innerHeight) - 0.5) * 25;
+        const moveX = ((clientX / innerWidth) - 0.5) * 15;
+        const moveY = ((clientY / innerHeight) - 0.5) * 15;
         image.style.transform = `translate(${moveX}px, ${moveY}px)`;
     });
 
     if (window.DeviceOrientationEvent) {
         window.addEventListener('deviceorientation', (event) => {
             const { beta, gamma } = event;
-            const moveX = gamma / 5;
-            const moveY = beta / 5;
+            const moveX = gamma / 15;
+            const moveY = beta / 15;
             image.style.transform = `translate(${moveX}px, ${moveY}px)`;
         });
     }
