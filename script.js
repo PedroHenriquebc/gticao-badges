9d1cda904aa5becd628c726f2c30719737d2c618
Pedro Henrique <pedrohbc96@gmail.com>
2024-07-26 21:06:41 -0300

    Adiciona rotação de imagem em mobile

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function changeImage(src) {
     document.getElementById('badge-image').src = src;
 }
+
+let counter = 0;
+const updateRate = 10;
+const limit = 45;
+const badge = document.getElementById("badge-image");
+
+function updateNow() {
+    return counter++ % updateRate === 0;
+};
+
+window.addEventListener("deviceorientation", function (event) {
+    if (updateNow()) {
+        let position = Math.round(event.gamma);
+        if (Math.abs(position) > limit) {
+            if (position > limit) {
+                position = limit;
+            } else {
+                position = -limit;
+            }
+        }
+        position = position / -100;
+        let style = "rotateY(" + position + "deg)";
+        badge.style.transform = style;
+    }
+});
